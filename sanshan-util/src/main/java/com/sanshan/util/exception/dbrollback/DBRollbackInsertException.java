bd067683ab3ab1732f985bd959ada2d2f2a9080d
SanShanYouJiu <www.85432173@qq.com>
2017-07-14 22:43:59 +0800

    之前没有上传成功的事务插入回滚异常

---
+++ sanshan-util/src/main/java/com/sanshan/util/exception/dbrollback/DBRollbackInsertException.java
@@ -0,0 +1,12 @@
+package com.sanshan.util.exception.dbrollback;
+
+public class DBRollbackInsertException extends Exception {
+
+    public DBRollbackInsertException(){
+
+    }
+    public DBRollbackInsertException(String message) {
+        super(message);
+    }
+
+}

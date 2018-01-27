267f0f6d454063cb4be2a07780f7d5b079f5712a
sanshanyoujiu <www.85432173@qq.com>
2018-01-27 14:51:41 +0800

    complete spring boot deploy

--- sanshan-web/src/main/Java/com/sanshan/Application.java
+++ sanshan-web/src/main/Java/com/sanshan/Application.java
@@ -1,19 +1,24 @@
 package com.sanshan;
 
-import com.alibaba.druid.spring.boot.autoconfigure.DruidDataSourceAutoConfigure;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.data.redis.RedisAutoConfiguration;
 import org.springframework.boot.autoconfigure.mongo.MongoAutoConfiguration;
 import org.springframework.boot.autoconfigure.thymeleaf.ThymeleafAutoConfiguration;
 import org.springframework.boot.autoconfigure.web.WebMvcAutoConfiguration;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
 
 /**
- * 开启需要Tomcat依赖
+ * 在主程序直接运行需要Tomcat依赖
  */
-@SpringBootApplication(exclude = {RedisAutoConfiguration.class, MongoAutoConfiguration.class,
-        DruidDataSourceAutoConfigure.class,ThymeleafAutoConfiguration.class, WebMvcAutoConfiguration.class})
-public class Application {
+@SpringBootApplication(exclude = {RedisAutoConfiguration.class, MongoAutoConfiguration.class, ThymeleafAutoConfiguration.class, WebMvcAutoConfiguration.class})
+public class Application  extends SpringBootServletInitializer {
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(Application.class);
+    }
 
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
--- sanshan-web/src/main/Java/com/sanshan/web/config/webaseconfig/WebAppInitializer.java
+++ sanshan-web/src/main/Java/com/sanshan/web/config/webaseconfig/WebAppInitializer.java
@@ -1,57 +1,58 @@
 package com.sanshan.web.config.webaseconfig;
 
-import org.springframework.web.filter.CharacterEncodingFilter;
-import org.springframework.web.servlet.support.AbstractAnnotationConfigDispatcherServletInitializer;
+import com.sanshan.Application;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
 
-import javax.servlet.FilterRegistration;
-import javax.servlet.ServletContext;
-import javax.servlet.ServletException;
-import javax.servlet.ServletRegistration;
-
-public class WebAppInitializer extends AbstractAnnotationConfigDispatcherServletInitializer {
-
-    @Override
-    protected Class<?>[] getRootConfigClasses() {
-        return new Class<?>[]{RootConfig.class};
-    }
-
-    @Override
-    protected Class<?>[] getServletConfigClasses() {
-        return new Class<?>[]{WebConfig.class};
-    }
-
-    @Override
-    protected String[] getServletMappings() {
-        return new String[]{"/api/*"};
-    }
-
+/**
+ * 如果使用Spring framework 构建 将这里的注释打开 然后将{@link Application}的继承
+ * {@link SpringBootServletInitializer}注释即可
+ * 默认采用Spring Boot 构建应用程序
+ *
+ */
+public class WebAppInitializer /* extends AbstractAnnotationConfigDispatcherServletInitializer*/ {
 
 //    @Override
-//    protected void customizeRegistration(ServletRegistration.Dynamic registration) {
-//        registration.setMultipartConfig(
-//                new MultipartConfigElement("D://测试", 2097152,4194304,0));
+//    protected Class<?>[] getRootConfigClasses() {
+//        return new Class<?>[]{RootConfig.class};
 //    }
-
-    @Override
-    public void onStartup(ServletContext servletContext) throws ServletException {
-         //
-        FilterRegistration.Dynamic filter = servletContext.addFilter("CharacterEncodingFilter",
-                CharacterEncodingFilter.class);
-        filter.setInitParameter("encoding", "utf-8");
-        filter.addMappingForUrlPatterns(null, false, "/api/*");
-
-//        主要负责处理由　JavaBeans Introspector的使用而引起的缓冲泄露
-        servletContext.addListener(org.springframework.web.util.IntrospectorCleanupListener.class);
 //
-        //log4j监听器
-        servletContext.addListener(org.springframework.web.util.Log4jConfigListener.class);
-
-        //Druid监控
-        ServletRegistration.Dynamic druidservlet = servletContext.addServlet("DruidStatView", com.alibaba.druid.support.http.StatViewServlet.class);
-        druidservlet.addMapping("/druid/*");
-
-        super.onStartup(servletContext);
-    }
+//    @Override
+//    protected Class<?>[] getServletConfigClasses() {
+//        return new Class<?>[]{WebConfig.class};
+//    }
+//
+//    @Override
+//    protected String[] getServletMappings() {
+//        return new String[]{"/api/*"};
+//    }
+//
+//
+////    @Override
+////    protected void customizeRegistration(ServletRegistration.Dynamic registration) {
+////        registration.setMultipartConfig(
+////                new MultipartConfigElement("D://测试", 2097152,4194304,0));
+////    }
+//
+//    @Override
+//    public void onStartup(ServletContext servletContext) throws ServletException {
+//         //
+//        FilterRegistration.Dynamic filter = servletContext.addFilter("CharacterEncodingFilter",
+//                CharacterEncodingFilter.class);
+//        filter.setInitParameter("encoding", "utf-8");
+//        filter.addMappingForUrlPatterns(null, false, "/api/*");
+//
+////        主要负责处理由　JavaBeans Introspector的使用而引起的缓冲泄露
+//        servletContext.addListener(org.springframework.web.util.IntrospectorCleanupListener.class);
+////
+//        //log4j监听器
+//        servletContext.addListener(org.springframework.web.util.Log4jConfigListener.class);
+//
+//        //Druid监控
+//        ServletRegistration.Dynamic druidservlet = servletContext.addServlet("DruidStatView", com.alibaba.druid.support.http.StatViewServlet.class);
+//        druidservlet.addMapping("/druid/*");
+//
+//        super.onStartup(servletContext);
+//    }
 
 
 }

b4f8cce3985b4b12305cd045ad1dcf38ff92c88a
sanshanyoujiu <www.85432173@qq.com>
2018-02-01 15:24:33 +0800

    fix es config issue

--- sanshan-web/src/main/Java/com/sanshan/Application.java
+++ sanshan-web/src/main/Java/com/sanshan/Application.java
@@ -2,6 +2,7 @@ package com.sanshan;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.data.elasticsearch.ElasticsearchAutoConfiguration;
 import org.springframework.boot.autoconfigure.data.redis.RedisAutoConfiguration;
 import org.springframework.boot.autoconfigure.mongo.MongoAutoConfiguration;
 import org.springframework.boot.autoconfigure.thymeleaf.ThymeleafAutoConfiguration;
@@ -12,7 +13,7 @@ import org.springframework.boot.web.support.SpringBootServletInitializer;
 /**
  * 在主程序直接运行需要Tomcat依赖
  */
-@SpringBootApplication(exclude = {RedisAutoConfiguration.class, MongoAutoConfiguration.class,ThymeleafAutoConfiguration.class, WebMvcAutoConfiguration.class})
+@SpringBootApplication(exclude = {RedisAutoConfiguration.class, MongoAutoConfiguration.class,ThymeleafAutoConfiguration.class, ElasticsearchAutoConfiguration.class, WebMvcAutoConfiguration.class})
 public class Application  extends SpringBootServletInitializer {
 
     @Override
--- sanshan-web/src/main/Java/com/sanshan/web/config/javaconfig/ElasticSearchConfig.java
+++ sanshan-web/src/main/Java/com/sanshan/web/config/javaconfig/ElasticSearchConfig.java
@@ -4,13 +4,12 @@ import org.elasticsearch.client.Client;
 import org.elasticsearch.client.transport.TransportClient;
 import org.elasticsearch.common.settings.Settings;
 import org.elasticsearch.common.transport.InetSocketTransportAddress;
-import org.springframework.beans.factory.annotation.Configurable;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
 import org.springframework.context.annotation.PropertySource;
 import org.springframework.context.support.PropertySourcesPlaceholderConfigurer;
-import org.springframework.data.elasticsearch.core.ElasticsearchOperations;
 import org.springframework.data.elasticsearch.core.ElasticsearchTemplate;
 import org.springframework.data.elasticsearch.repository.config.EnableElasticsearchRepositories;
 
@@ -19,10 +18,9 @@ import java.net.InetAddress;
 /**
  *
  */
-@Configurable
+@Configuration
 @EnableElasticsearchRepositories(basePackages = "com.sanshan.dao.elastic")
 @PropertySource("file:D:/SanShanBlog.properties")
-@ComponentScan("com.sanshan.dao.elastic")
 public class ElasticSearchConfig {
 
     @Value("${elasticsearch.host}")
@@ -50,8 +48,10 @@ public class ElasticSearchConfig {
                         new InetSocketTransportAddress(InetAddress.getByName(EsHost), EsPort));
     }
 
+
     @Bean
-    public ElasticsearchOperations elasticsearchTemplate() throws Exception {
+    @Primary
+    public ElasticsearchTemplate elasticsearchTemplate() throws Exception {
         return new ElasticsearchTemplate(client());
     }
 

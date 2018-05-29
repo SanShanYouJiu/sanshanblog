f48cdb17745ed2d1cbf8b19a2dbba01d69e7d23f
sanshan <sanshan@maihaoche.com>
2018-05-29 23:29:16 +0800

    update TODO.md

---
+++ sanshan-main/sanshan-main-service/src/main/java/xyz/sanshan/main/service/BrowseService.java
@@ -0,0 +1,38 @@
+package xyz.sanshan.main.service;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import xyz.sanshan.main.service.editor.BlogIdGenerate;
+
+/**
+ * 用户浏览
+ * @author sanshan <sanshan@maihaoche.com>
+ * @date 2018-05-29
+ */
+@Service
+@Slf4j
+public class BrowseService {
+
+    @Autowired
+    private BlogIdGenerate blogIdGenerate;
+
+    /**
+     * 通过BlogId获取浏览数
+     * @param blogId
+     * @return
+     */
+    public Long getBlogBrowsesForBlogId(Long blogId){
+        return null;
+    }
+
+    /**
+     * 加入BlogId对应的博客浏览数
+     * @param blogId
+     */
+    public void BlogBrowsesAddForBlogId(Long blogId){
+
+    }
+
+
+}

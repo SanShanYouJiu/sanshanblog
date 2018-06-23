d857d25ca372bf870e2757f12df2a652d0db16cb
sanshan <sanshan@maihaoche.com>
2018-06-23 10:37:04 +0800

    auth-server slow start bug fix

--- sanshan-auth/sanshan-auth-server/src/main/java/xyz/sanshan/auth/security/server/config/AopConfiguration.java
+++ sanshan-auth/sanshan-auth-server/src/main/java/xyz/sanshan/auth/security/server/config/AopConfiguration.java
@@ -1,13 +1,11 @@
 package xyz.sanshan.auth.security.server.config;
 
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
 import xyz.sanshan.auth.security.server.util.ControllerAop;
 
-@Configuration
+//@Configuration
 public class AopConfiguration {
 
-    @Bean
+    //@Bean
     public ControllerAop controllerAop() {
 
         return new ControllerAop();
